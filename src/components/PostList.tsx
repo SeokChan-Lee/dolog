6920be76dc7b2a4aef97349dac3eaf029b040d09
SeokChan-Lee <melee0826@gmail.com>
2025-05-06 23:29:03 +0900

    🐛 Bug : any 타입 변경

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,13 +1,20 @@
 import { getDatabase } from "@/lib/notion";
+import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 export default async function PostList() {
   const response = await getDatabase();
 
+  const pages = response.results.filter(
+    (page): page is PageObjectResponse => "properties" in page
+  );
+
   return (
     <ul className="space-y-4">
-      {response.results.map((page: any) => {
+      {pages.map((page) => {
         const title =
-          page.properties?.Title?.title?.[0]?.plain_text || "제목 없음";
+          page.properties?.Title?.type === "title"
+            ? page.properties.Title.title[0]?.plain_text
+            : "제목 없음";
 
         return (
           <li key={page.id} className="border-b pb-2">
