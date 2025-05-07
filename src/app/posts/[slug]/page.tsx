2192f9fd6f67c0530312f5b909174100ea7d4f1c
SeokChan-Lee <melee0826@gmail.com>
2025-05-07 22:53:26 +0900

    🐛 Bug : 빌드 오류 수정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,12 +2,11 @@ import { getDatabase, getPageContentBySlug } from "@/lib/notion";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import { NotionRenderer } from "@/components/NotionRenderer";
-import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import { formatDate } from "@/utils/formatDate";
+import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 export async function generateStaticParams() {
   const response = await getDatabase();
-
   const pages = response.results.filter(
     (page): page is PageObjectResponse => "properties" in page
   );
@@ -26,25 +25,23 @@ export async function generateStaticParams() {
 export async function generateMetadata({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }): Promise<Metadata> {
-  const { slug } = params;
+  const { slug } = await params;
   const content = await getPageContentBySlug(slug);
-
   if (!content) return {};
-
   return {
     title: content.title,
     description: content.excerpt || "Notion Post Detail",
   };
 }
 
-export default async function PostPage({
+export default async function Page({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = params;
+  const { slug } = await params;
   const pageData = await getPageContentBySlug(slug);
 
   if (!pageData) return notFound();
@@ -52,7 +49,9 @@ export default async function PostPage({
   return (
     <main className="max-w-3xl mx-auto px-4 py-10">
       <h1 className="text-4xl font-bold mb-6">{pageData.title}</h1>
-      <p className="text-sm text-gray-500 mb-6">{formatDate(pageData.date)}</p>
+      <p className="text-sm text-gray-500 mb-6">
+        {formatDate(pageData.date)} · by {pageData.author}
+      </p>
       <NotionRenderer blocks={pageData.blocks} />
     </main>
   );
