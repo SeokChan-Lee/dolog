aae2ef8b706685497d79c7d2fdf47cac56ec7946
SeokChan-Lee <melee0826@gmail.com>
2025-05-07 21:53:18 +0900

    ✨ Feat : 상세페이지 링크 추가

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,6 @@
 import { getDatabase } from "@/lib/notion";
 import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import Link from "next/link";
 
 export default async function PostList() {
   const response = await getDatabase();
@@ -11,14 +12,24 @@ export default async function PostList() {
   return (
     <ul className="space-y-4">
       {pages.map((page) => {
+        const titleProp = page.properties?.Title;
+        const slugProp = page.properties?.Slug;
+
         const title =
-          page.properties?.Title?.type === "title"
-            ? page.properties.Title.title[0]?.plain_text
+          titleProp?.type === "title" && titleProp.title.length > 0
+            ? titleProp.title[0].plain_text
             : "제목 없음";
+
+        const slug =
+          slugProp?.type === "rich_text" && slugProp.rich_text.length > 0
+            ? slugProp.rich_text[0].plain_text
+            : "";
 
         return (
           <li key={page.id} className="border-b pb-2">
-            {title}
+            <Link href={`/posts/${slug}`} className=" hover:underline">
+              {title}
+            </Link>
           </li>
         );
       })}
