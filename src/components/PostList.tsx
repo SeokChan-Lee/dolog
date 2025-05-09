bf0f89197cd00aa7b0e0cf0740e166ea2fb1a667
SeokChan-Lee <melee0826@gmail.com>
2025-05-09 19:25:40 +0900

    ♻️ Refactor : PostList 카테고리페이지에도 사용가능하게 리팩토링

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,23 +1,16 @@
-import { getDatabase } from "@/lib/notion";
 import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import Link from "next/link";
 import Image from "next/image";
 import { formatDate } from "@/utils/formatDate";
 
-export default async function PostList() {
-  const response = await getDatabase();
-
-  const pages = response.results.filter(
-    (page): page is PageObjectResponse => "properties" in page
-  );
-
+export default function PostList({ posts }: { posts: PageObjectResponse[] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-      {pages.map((page) => {
+      {posts.map((page) => {
         const titleProp = page.properties?.Title;
         const slugProp = page.properties?.Slug;
-        const coverProp = page.cover;
         const dateProp = page.properties?.Date;
+        const coverProp = page.cover;
 
         const title =
           titleProp?.type === "title" && titleProp.title.length > 0
@@ -29,7 +22,8 @@ export default async function PostList() {
             ? slugProp.rich_text[0].plain_text
             : "";
 
-        const date = dateProp?.type === "date" ? dateProp.date?.start : "";
+        const date =
+          dateProp?.type === "date" ? dateProp.date?.start || "" : "";
 
         const coverImage =
           coverProp?.type === "external"
@@ -42,14 +36,14 @@ export default async function PostList() {
           <Link
             href={`/posts/${slug}`}
             key={page.id}
-            className="block rounded overflow-hidden mx-5 sm:mx-0 "
+            className="block rounded overflow-hidden mx-5 sm:mx-0 hover:scale-105 transition-transform"
           >
             <Image
               src={coverImage}
               alt={title}
               width={600}
               height={300}
-              className="w-full h-48 object-cover transition-transform duration-200 hover:scale-105"
+              className="w-full h-48 object-cover"
             />
             <div className="p-4">
               <h2 className="text-xl font-semibold">{title}</h2>
