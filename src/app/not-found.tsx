23d84c725b5b447e4a139d77b0469abba51fdd7b
SeokChan-Lee <melee0826@gmail.com>
2025-08-11 20:42:47 +0900

    ✨ Feat : 404페이지 물음표 이미지 추가 및 홈으로 돌아가기 버튼 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function NotFound() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="h-screen w-screen flex items-center justify-center flex-col gap-5">
+      <Image
+        src={"/assets/icon/dot_question.png"}
+        width={300}
+        height={300}
+        alt="물음표 이미지"
+      />
       <span className="font-bold text-l md:text-xl lg:text-2xl">
         페이지를 찾을 수 없습니다.
       </span>
+      <Link href={"/"}>
+        <span className="cursor-pointer">홈으로 돌아가기</span>
+      </Link>
     </div>
   );
 }
