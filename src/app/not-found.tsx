7d32c4d84264b145ed4059b0cd4ad9f7e6966eea
SeokChan-Lee <melee0826@gmail.com>
2025-08-11 20:29:36 +0900

    ✨ feat : 404 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <span className="font-bold text-l md:text-xl lg:text-2xl">
+        페이지를 찾을 수 없습니다.
+      </span>
+    </div>
+  );
+}
