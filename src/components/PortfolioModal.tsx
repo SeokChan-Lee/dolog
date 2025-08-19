fb83c05983d6a7947d28f7de425e7e190ab3c8e3
SeokChan-Lee <melee0826@gmail.com>
2025-08-20 01:34:54 +0900

    ✨ Feat : 포트폴리오 모달 컴포넌트 구현

---
+++ src/components/PortfolioModal.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Modal from "./Modal";
+import { useState } from "react";
+
+export default function PortfolioModal() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => setIsOpen(true)}
+        className="text-xl font-bold  transition-transform duration-200 hover:scale-105 text-black bg-blue-200 p-2 rounded-xl"
+      >
+        Portfolio
+      </button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <p>내용 추가 및 플로팅 버튼으로 변경 예정</p>
+        <p>내용 추가 및 플로팅 버튼으로 변경 예정</p>
+        <p>내용 추가 및 플로팅 버튼으로 변경 예정</p>
+        <p>내용 추가 및 플로팅 버튼으로 변경 예정</p>
+        <p>내용 추가 및 플로팅 버튼으로 변경 예정</p>
+        <p>내용 추가 및 플로팅 버튼으로 변경 예정</p>
+        <p>
+          내용 추가 및 플로팅 버튼으로 변경 예정내용 추가 및 플로팅 버튼으로
+          변경 예정내용 추가 및 플로팅 버튼으로 변경 예정내용 추가 및 플로팅
+          버튼으로 변경 예정
+        </p>
+      </Modal>
+    </>
+  );
+}
