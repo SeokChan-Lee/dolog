9beee84c9d056662986731a809f645c6f39a14b2
SeokChan-Lee <melee0826@gmail.com>
2025-09-04 18:02:06 +0900

    💬 Chore : 내용 임시 추가

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -23,7 +23,7 @@ export default function ProfileModal() {
       </button>
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <div className="lg:flex lg:justify-between ">
-          <section className="mb-15 lg:mb-0 grid grid-cols-1 gap-8">
+          <section className="mb-15 lg:mb-0 grid grid-cols-1 gap-8 h-[500px]">
             <p className="text-4xl font-bold">이석찬</p>
             <p className="text-2xl">프론트엔드 개발자</p>
             <p className="text-lg">
@@ -31,7 +31,7 @@ export default function ProfileModal() {
               이석찬입니다.
             </p>
           </section>
-          <section className="grid grid-cols-1 gap-8">
+          <section className="grid grid-cols-1 gap-8 border-l-3 border-l-blue-200 pl-4">
             <div>
               <p className={SEMI_TITLE}>소개</p>
             </div>
@@ -44,6 +44,22 @@ export default function ProfileModal() {
             </div>
             <div>
               <p className={SEMI_TITLE}>프로젝트</p>
+              <div className="flex flex-col">
+                <span>위리브</span>
+                <span>
+                  2025.05 ~ 2025.06 (4주)
+                  <br />
+                  역할 : 팀프로젝트(팀장) / FE(3명), BE(2명)
+                </span>
+
+                <span>
+                  위리브는 아파트 거주 주민들과 관리 주체를 위한 상호
+                  커뮤니케이션 및 행정 관리 플랫폼입니다.
+                  <br />
+                  해당 서비스는 공지사항 전달, 민원 접수, 투표, 입주 승인 등
+                  공동체 운영에 필수적인 기능들을 온라인화합니다.
+                </span>
+              </div>
             </div>
             <div>
               <p className={SEMI_TITLE}>교육 이력</p>
