8dc2179091968ad770e81a10bdc6440cf76e3d9c
SeokChan-Lee <melee0826@gmail.com>
2025-09-06 17:08:08 +0900

    ♻️ Refactor : 인턴 프롭 추가 및 스타일 수정

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,6 +7,9 @@ interface ProjectProps {
   introduction: React.ReactNode;
   deployLinkAddress: string;
   deployLink: string;
+  isIntern?: boolean;
+  isRepoLink?: boolean;
+  repoAddress?: string;
 }
 
 export default function Project({
@@ -16,10 +19,16 @@ export default function Project({
   introduction,
   deployLinkAddress,
   deployLink,
+  isIntern = false,
+  isRepoLink = false,
+  repoAddress,
 }: ProjectProps) {
   return (
     <article className="flex flex-col">
-      <h2 className="text-xl font-semibold">{projectName}</h2>
+      <div className="flex items-end gap-1">
+        <h2 className="text-xl font-semibold">{projectName}</h2>
+        {isIntern && <p className="text-sm text-[#808fa9]">(인턴)</p>}
+      </div>
       <p className="text-sm mb-4 text-[#808fa9]">
         {period}
         <br />
@@ -28,20 +37,38 @@ export default function Project({
 
       <p className="mb-4 text-[#808fa9]">{introduction}</p>
 
-      <a
-        href={deployLinkAddress}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:underline flex bg-blue-200 w-fit rounded-lg px-2 py-1 text-black gap-1 items-center text-sm"
-      >
-        <Image
-          src="/assets/icon/link_icon.png"
-          alt="링크 아이콘"
-          width={20}
-          height={20}
-        />
-        {deployLink}
-      </a>
+      <div className="flex gap-5">
+        <a
+          href={deployLinkAddress}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline flex bg-blue-200 w-fit rounded-lg px-2 py-1 text-black gap-1 items-center text-sm"
+        >
+          <Image
+            src="/assets/icon/link_icon.png"
+            alt="링크 아이콘"
+            width={20}
+            height={20}
+          />
+          {deployLink}
+        </a>
+        {isRepoLink && (
+          <a
+            href={repoAddress}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline flex bg-blue-200 w-fit rounded-lg px-2 py-1 text-black gap-2 items-center text-sm"
+          >
+            <Image
+              src="/assets/icon/git_icon.png"
+              alt="깃 아이콘"
+              width={20}
+              height={20}
+            />
+            Repo
+          </a>
+        )}
+      </div>
     </article>
   );
 }
