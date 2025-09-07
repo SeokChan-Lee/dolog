da7a0561981039b39897f7dce717bd8a862c5334
SeokChan-Lee <melee0826@gmail.com>
2025-09-07 23:08:12 +0900

    ✨ Feat : Chip 컴포넌트 구현

---
+++ src/components/Chip.tsx
@@ -0,0 +1,14 @@
+interface ChipProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Chip({ children, className }: ChipProps) {
+  return (
+    <span
+      className={`inline-flex items-center bg-[#111827] text-[#808fa9] text-sm font-medium px-3 py-1 rounded-full border-1 ${className} ${className}`}
+    >
+      {children}
+    </span>
+  );
+}
