678549f5aa2ee34d235eecd8a066913497f425a6
SeokChan-Lee <melee0826@gmail.com>
2025-09-11 22:03:30 +0900

    ✨ Feat : GitHub 링크 추가

--- src/components/GitLinkButton.tsx
+++ src/components/GitLinkButton.tsx
@@ -3,14 +3,20 @@ import Button from "./Button";
 
 export default function GitLinkButton() {
   return (
-    <Button className="mb-2 h-[56px]">
-      <Image
-        src="/assets/icon/git_icon.png"
-        alt="깃 아이콘"
-        width={35}
-        height={35}
-      />
-      <p>GitHub</p>
-    </Button>
+    <a
+      href="https://github.com/SeokChan-Lee"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Button className="mb-2 h-[56px]">
+        <Image
+          src="/assets/icon/git_icon.png"
+          alt="깃 아이콘"
+          width={35}
+          height={35}
+        />
+        <p>GitHub</p>
+      </Button>
+    </a>
   );
 }
