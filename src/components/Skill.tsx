cecde353c384f4f705bf4ff33a35a498a836e799
SeokChan-Lee <melee0826@gmail.com>
2025-09-11 22:06:08 +0900

    💄 UI : 스킬 부분 줄바꿈 추가

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -3,7 +3,7 @@ import Chip from "./Chip";
 export default function Skill() {
   return (
     <>
-      <section className="flex gap-2 mb-2">
+      <section className="flex flex-wrap gap-2 mb-2">
         <Chip>React</Chip>
         <Chip>Next.js</Chip>
         <Chip>TypeScript</Chip>
@@ -11,7 +11,7 @@ export default function Skill() {
         <Chip>HTML</Chip>
         <Chip>CSS</Chip>
       </section>
-      <section className="flex gap-2">
+      <section className="flex flex-wrap gap-2">
         <Chip>Tailwind CSS</Chip>
         <Chip>Styled ComponentsReact</Chip>
       </section>
