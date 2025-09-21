3330a35895f24522bd0dc1c8b0229620d9216766
SeokChan-Lee <melee0826@gmail.com>
2025-09-21 20:57:49 +0900

    ✏️ Chore : 칩 컴포넌트 오타 수정

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -6,7 +6,7 @@ interface ChipProps {
 export default function Chip({ children, className }: ChipProps) {
   return (
     <span
-      className={`inline-flex items-center bg-[#111827] text-[#808fa9] text-sm font-medium px-3 py-1 rounded-full border-1 ${className} ${className}`}
+      className={`inline-flex items-center bg-[#111827] text-[#808fa9] text-sm font-medium px-3 py-1 rounded-full border-1 ${className}`}
     >
       {children}
     </span>
