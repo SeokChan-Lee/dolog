d98257c0d4b6ad3e274d4a71728c7e730af461fe
SeokChan-Lee <melee0826@gmail.com>
2025-09-24 20:45:34 +0900

    💄 UI : 포스트 제목 <> 내용 구분선 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -48,8 +48,10 @@ export default async function Page({
 
   return (
     <main className="max-w-3xl mx-auto py-40 px-7 md:px-0">
-      <h1 className="text-4xl font-bold mb-1">{pageData.title}</h1>
-      <p className="text-md text-gray-500 mb-10">{formatDate(pageData.date)}</p>
+      <h1 className="text-4xl font-bold mb-2">{pageData.title}</h1>
+      <p className="text-md text-gray-500 mb-10 border-b-2 pb-10">
+        {formatDate(pageData.date)}
+      </p>
       <NotionRenderer blocks={pageData.blocks} />
     </main>
   );
